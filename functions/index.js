7ea509f1f28d30ddbf0b0fe35aeb72461ba7cdfa
Ryan Kirkpatrick <rk168810@gmail.com>
2021-02-19 03:07:24 -0800

    Setup website using express-handlebars

---
+++ functions/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.status(200).render('homepage');
+});
+
+app.get('/:pageTitle', (req, res) => {
+	res.status(200).render(req.params.pageTitle, {
+		pageTitle: req.params.pageTitle
+	});
+});
+
+app.get('*', (req, res) => {
+	res.status(404).render('404', {
+		pageTitle: "Error"
+	});
+});
+
+// Define the Firebase function that will act as Express application
+// Note: This `app` must match with `/firebase.json` rewrites rule.
+exports.app = functions.https.onRequest(app);
