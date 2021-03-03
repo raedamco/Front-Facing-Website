47ac2324af9fc1036f9d693c78dd020473452a2a
Ryan Kirkpatrick <rk168810@gmail.com>
2021-03-03 03:00:01 -0800

    Added javascript to submit forms via post and added google sheets.
    + Added javascript to submit forms using the post method.
    + Added google sheets API for the IOS early access form.

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,42 @@
 'use strict';
 
+// Firebase function for node server
 const functions = require('firebase-functions');
+// Express for routing and Handlebars for templating
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
-const google = require('googleapis');
+// For Google Sheets API
+const {google} = require('googleapis');
+// Fro string formatting
+const util = require('util');
+// Validator for form input
+const {body, validationResult} = require('express-validator');
 
+// Setup express/handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+//app.use(express.urlencoded({extended:true}));
+//app.use(express.json());
 
+// Catch unhandled promises
+process.on('unhandledRejection', error => {
+	// Will print "unhandledRejection err is not defined"
+	console.log('unhandledRejection', error.message);
+});
+
+// Express routes
+// - Landing page
 app.get('/', (req, res) => {
 	res.status(200).render('homepage');
 });
 
+// - Other pages
 app.get('/:pageTitle', (req, res, next) => {
+	let pageTitle = parsePageTitle(req.params.pageTitle);
 	res.status(200).render(req.params.pageTitle, {
-		pageTitle: req.params.pageTitle
+		pageTitle: pageTitle
 	}, (err, html) => {
 		if (err) {
 			next();
@@ -26,6 +46,28 @@ app.get('/:pageTitle', (req, res, next) => {
 	});
 });
 
+// - Forms
+app.post('/early-access-form',
+body('email').isEmail(),
+body('firstname').isLength({min: 1}),
+body('lastname').isLength({min: 1}),
+(req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		console.log(errors);
+		return res.status(400).json({ errors: errors.array() });
+	}
+	console.log("Form submitted successfully.");
+	let jwt = getJwt();
+	let apiKey = getApiKey();
+	let spreadsheetId = '1I2KGPmvnKbWW8tOTfdGlrgvwI3Il9zrAKx9Xs6WY05Y';		// TODO Replace with real id. This one is a test spreadsheet.
+	let range = 'A1:B3';
+	let row = [getDate(), req.body.firstname + " " + req.body.lastname, req.body.email];
+	appendSheetRow(jwt, apiKey, spreadsheetId, range, row);
+	res.status(200).type('text/plain').end('OK');
+});
+
+// - Error page
 app.get('*', (req, res) => {
 	res.status(404).render('404', {
 		pageTitle: "Error"
@@ -36,20 +78,21 @@ app.get('*', (req, res) => {
 // Note: This `app` must match with `/firebase.json` rewrites rule.
 exports.app = functions.https.onRequest(app);
 
-// Google Sheets API
-exports.reply = (req, res) => {
-	let jwt = getJwt();
-	let apiKey = getApiKey();
-	let spreadsheetId = '1I2KGPmvnKbWW8tOTfdGlrgvwI3Il9zrAKx9Xs6WY05Y';		// TODO Replace with real id. This one is a test spreadsheet.
-	let range = 'A1';
-	let row = [new Date(), 'A Cloud Function was here'];
-	appendSheetRow(jwt, apiKey, spreadsheetId, range, row);
-	res.status(200).type('text/plain').end('OK');
-};
+// Helper functions
+function parsePageTitle(pageTitle) {
+	pageTitle = pageTitle.replace("-", " ");
+	pageTitle.replace("%20", " ");
+	let splitStr = pageTitle.toLowerCase().split(' ');
+	for (let i = 0; i < splitStr.length; i++) {
+		splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+	}
+	return splitStr.join(' ');
+}
 
+// Google Sheets API
 function getJwt() {
 	let credentials = require("./credentials.json");
-	return new google.Auth.JWT(
+	return new google.auth.JWT(
 		credentials.client_email, null, credentials.private_key,
 		['https://www.googleapis.com/auth/spreadsheets']
 	);
@@ -58,6 +101,19 @@ function getJwt() {
 function getApiKey() {
 	let apiKeyFile = require("./api_key.json");
 	return apiKeyFile.key;
+}
+
+function getDate() {
+	let dateObj = new Date();
+	// current date
+	// adjust 0 before single digit date
+	let date = ("0" + dateObj.getDate()).slice(-2);
+	let month = ("0" + (dateObj.getMonth() + 1)).slice(-2);
+	let year = dateObj.getFullYear();
+	let hours = dateObj.getHours();
+	let minutes = ("0" + dateObj.getMinutes()).slice(-2);
+	let seconds = ("0" + dateObj.getSeconds()).slice(-2);
+	return util.format("%s/%s/%s %s:%s:%s", month, date, year, hours, minutes, seconds);
 }
 
 function appendSheetRow(jwt, apiKey, spreadsheetId, range, row) {
@@ -75,7 +131,7 @@ function appendSheetRow(jwt, apiKey, spreadsheetId, range, row) {
 		}
 		else
 		{
-			console.log('Updated sheet: ' + result.data.updates.updatedRange);
+			console.log('Updated: ' + res.data.updates.updatedRange);
 		}
 	});
 }
