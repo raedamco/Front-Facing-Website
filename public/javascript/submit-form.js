7c27793426a3351421e1449747d62d60b4ad7fcc
Ryan Kirkpatrick <Rk168810@gmail.com>
2021-07-10 01:20:53 -0700

    Added todo to fix form submission

--- public/javascript/submit-form.js
+++ public/javascript/submit-form.js
@@ -2,6 +2,7 @@
  * @author Ryan Dean Kirkpatrick
  */
 
+//TODO fix for multiple forms on one page. use form id for postUrl
 window.addEventListener("load", () => {
 	function sendForm(form) {
 		const formData = new FormData(form);
@@ -14,57 +15,58 @@ window.addEventListener("load", () => {
 			data: json,
 			success: (data) => {
 				// Display confirmation message to user.
-				if (typeof(confirmationMsg) !== undefined)
-				{
+				if (typeof confirmationMsg !== undefined) {
 					// Display default message if custom message is missing.
 					CreateMessage("confirmation", "<p>Thank you!</p>");
-				}
-				else
-				{
+				} else {
 					// Add custom message to page with:
 					// <script>let confirmationMsg = "<p>Thank you!</p>";</script>
 					CreateMessage("confirmation", confirmationMsg);
 				}
-				analytics.logEvent('form_success', {postUrl: postUrl, data: json});
+				analytics.logEvent("form_success", { postUrl: postUrl, data: json });
 				form.reset();
 			},
 			error: (data) => {
 				// Combine error messages from the node server into a single string.
 				let errorMsg = "<p><strong>Try Again:</strong>";
 				let errors = data.responseJSON.errors;
-				for(let i = 0; i < errors.length; i++)
-				{
+				for (let i = 0; i < errors.length; i++) {
 					errorMsg += "<br />" + errors[i].msg;
 				}
 				errorMsg += "</p>";
 				// Display error message to user.
 				CreateMessage("tryagain", errorMsg);
-				analytics.logEvent('form_error', {postUrl: postUrl, data: json, error: errorMsg});
+				analytics.logEvent("form_error", {
+					postUrl: postUrl,
+					data: json,
+					error: errorMsg,
+				});
 			},
-			contentType: 'application/json'
+			contentType: "application/json",
 		});
 	}
 
 	// Get reCaptcha token
 	function getToken(form) {
 		//Generate token
-		if (recaptchaKey) {		// reCaptchaKey set in main.handlebars
-			grecaptcha.execute(recaptchaKey, { action: 'form' })
-			.then((token) => {
+		if (recaptchaKey) {
+			// reCaptchaKey set in main.handlebars
+			grecaptcha.execute(recaptchaKey, { action: "form" }).then((token) => {
 				let tokenInput = document.getElementById("token");
-				if(tokenInput)
-				{
+				if (tokenInput) {
 					tokenInput.value = token;
-				}
-				else
-				{
+				} else {
 					// Append reCaptcha token input if it is missing
-					$(form).append("<input type='hidden' id='token' name='token' value='" + token + "'/>");
+					$(form).append(
+						"<input type='hidden' id='token' name='token' value='" +
+							token +
+							"'/>"
+					);
 				}
 				// Send Form to server
 				sendForm(form);
 				Promise.resolve();
-			})
+			});
 		}
 	}
 
@@ -72,12 +74,11 @@ window.addEventListener("load", () => {
 	const form = document.querySelector("form");
 
 	// Override form default submission
-	if (form != null)
-	{
-		form.addEventListener('submit', (event) => {
+	if (form != null) {
+		form.addEventListener("submit", (event) => {
 			event.preventDefault();
 
 			getToken(form);
 		});
 	}
-})
+});
